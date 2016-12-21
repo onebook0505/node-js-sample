1f4c8fd8deabf5130b200ee245b79db6b33b910c
Matt <onebook0505@gmail.com>
2016-12-21 22:00:22 +0800

    3

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('test dokku!')
+  response.send('test dokkuuu!')
 })
 
 app.listen(app.get('port'), function() {
